36dc46ae97ec5e68cd3c16a3732b69697c717f8a
plsuwu <pls@plsuwu.com>
2024-05-09 23:28:13 +1000

    initial commit

---
+++ project.py
@@ -0,0 +1,19 @@
+import graphs
+import digraphs
+import csv
+
+def gamesOK(games):
+    print(games)
+    return
+
+def referees(games, refereecsvfilename):
+   pass
+
+def gameGroups(assignedReferees):
+   pass
+
+def gameSchedule(assignedReferees, gameGroups):
+   pass
+
+def scores(p, s, c, games):
+   pass
